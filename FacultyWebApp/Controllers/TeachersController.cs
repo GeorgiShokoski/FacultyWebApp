7557c20d835555a6d29bfecda17c20590263ca89
Georgi Sokoski <georgi_shokoski@hotmail.com>
2022-05-26 12:15:06 +0200

    finishing details

--- FacultyWebApp/Controllers/TeachersController.cs
+++ FacultyWebApp/Controllers/TeachersController.cs
@@ -9,15 +9,20 @@ using FacultyWebApp.Data;
 using FacultyWebApp.Models;
 using FacultyWebApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using FacultyWebApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FacultyWebApp.Controllers
 {
     public class TeachersController : Controller
     {
+        private UserManager<FacultyWebAppUser> _userManager;
         private readonly FacultyWebAppContext _context;
 
-        public TeachersController(FacultyWebAppContext context)
+        public TeachersController(FacultyWebAppContext context, UserManager<FacultyWebAppUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
@@ -209,6 +214,48 @@ namespace FacultyWebApp.Controllers
             _context.Teacher.Remove(teacher);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateNewAccount()
+        {
+            var teachers = _context.Teacher.AsEnumerable();
+            teachers = teachers.OrderBy(s => s.FullName);
+            CreateNewAccountVM viewmodel = new CreateNewAccountVM
+            {
+                TeacherList = new MultiSelectList(teachers, "TeacherId", "FullName")
+            };
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateNewAccount(CreateNewAccountVM viewmodel)
+        {
+            
+            var name = "";
+            
+            IList<int> TList = viewmodel.SelectedTeacher.ToList();
+            Teacher teacher = _context.Teacher.Single(x => x.TeacherId == TList[0]);
+            name = teacher.FirstName.ToLower() + teacher.LastName.ToLower();
+            if (ModelState.IsValid)
+            {
+                FacultyWebAppUser user = await _userManager.FindByEmailAsync(name + "@schoolapp.com");
+                if (user == null)
+                {
+                    var User = new FacultyWebAppUser();
+                    User.Email = name.ToString() + "@faculty.com";
+                    User.UserName = name.ToString() + "@faculty.com";
+                    string userPWD = name.ToString() + "A123";
+                    IdentityResult chkUser = await _userManager.CreateAsync(User, userPWD);
+                    //Add default User to Role Admin
+                    if (chkUser.Succeeded) { var result1 = await _userManager.AddToRoleAsync(User, "Professor"); }
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+
         }
 
         private bool TeacherExists(int id)
---
+++ FacultyWebApp/ViewModels/CreateNewAccountVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FacultyWebApp.ViewModels
+{
+    public class CreateNewAccountVM
+    {
+        public IEnumerable<SelectListItem>? TeacherList { get; set; }
+        public IEnumerable<int>? SelectedTeacher { get; set; }
+    }
+}
